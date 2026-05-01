6d3dc6bef16b683f3aa87b2cbda0c704df7de49d
rolex-creator <k8810615@gmail.com>
2026-05-02 01:27:05 +0530

    Improve MongoDB connection handling and logging
    Refactor MongoDB connection and server start logic.

--- server.js
+++ server.js
@@ -115,6 +115,15 @@ app.delete('/api/admin/user/:id', async (req, res) => {
     res.send("Deleted");
 });
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(process.env.PORT, () => console.log('Server running on port ' + process.env.PORT));
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB connected");
+  app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+  });
+})
+.catch((err) => {
+  console.error("MongoDB error:", err);
 });
